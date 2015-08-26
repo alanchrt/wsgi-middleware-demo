6bf1908acce0df1d26634469f783ff8df0c685c0
Alan Christopher Thomas <alanctkc@gmail.com>
2015-08-26 16:10:30 -0500

    add a response timing middleware outside the cors middleware

---
+++ middleware/timing_middleware.py
@@ -0,0 +1,16 @@
+import time
+
+
+class ResponseTimingMiddleware(object):
+    """A wrapper around an app to print out the response time for each
+    request."""
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        """Meaure the time spent in the application."""
+        start_time = time.time()
+        response = self.app(environ, start_response)
+        response_time = (time.time() - start_time) / 1000
+        print "Response time: {:.10f}ms".format(response_time)
+        return response
--- serve_api.py
+++ serve_api.py
@@ -2,10 +2,11 @@ from wsgiref.simple_server import make_server
 
 from apps.api_endpoint import application
 from middleware.cors_middleware import CORSMiddleware
+from middleware.timing_middleware import ResponseTimingMiddleware
 
 # Run a wsgi server to serve the app
 print "Serving api on port 8000..."
-wrapped_application = CORSMiddleware(
-    application, whitelist=['http://localhost:9000', 'http://localhost:9001'])
+wrapped_application = ResponseTimingMiddleware(CORSMiddleware(
+    application, whitelist=['http://localhost:9000', 'http://localhost:9001']))
 httpd = make_server('localhost', 8000, wrapped_application)
 httpd.serve_forever()
