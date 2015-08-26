d8046e25406e341c574fcef0b3999f070043afe0
Alan Christopher Thomas <alanctkc@gmail.com>
2015-08-26 13:33:43 -0500

    add a custom application in the middleware to wrap the specified wsgi application with some behavior

--- middleware/cors_middleware.py
+++ middleware/cors_middleware.py
@@ -1,3 +1,8 @@
 def cors_middleware(app):
     print "CORS middleware applied."
-    return app
+
+    def cors_enabled_application(environ, start_response):
+        print "Do some stuff here."
+        return app(environ, start_response)
+
+    return cors_enabled_application
