e7c9181af26d493a7d4db7a836dc7105be3d086c
Alan Christopher Thomas <alanctkc@gmail.com>
2015-08-26 14:09:44 -0500

    refactor into class for improved readabilty and testability

--- middleware/cors_middleware.py
+++ middleware/cors_middleware.py
@@ -1,25 +1,31 @@
-def validate_origin(origin, whitelist):
-    return origin and origin in whitelist
+class CORSMiddleware(object):
+    def __init__(self, app, whitelist=None):
+        """Initialize the middleware for the specified app."""
+        if whitelist is None:
+            whitelist = []
+        self.app = app
+        self.whitelist = whitelist
 
+    def validate_origin(self, origin):
+        """Validate that the origin of the request is whitelisted."""
+        return origin and origin in self.whitelist
 
-def cors_middleware(app, whitelist=None):
-    print "CORS middleware applied."
-
-    if whitelist is None:
-        whitelist = []
-
-    def cors_enabled_application(environ, start_response):
-
-        origin = environ.get('HTTP_ORIGIN')
-
+    def cors_response_factory(self, origin, start_response):
+        """Create a start_response method that includes a CORS header for the
+        specified origin."""
         def cors_allowed_response(status, response_headers, exc_info=None):
             """This wraps the start_response behavior to add some headers."""
             response_headers.extend([('Access-Control-Allow-Origin', origin)])
             return start_response(status, response_headers, exc_info)
 
-        if validate_origin(origin, whitelist):
-            return app(environ, cors_allowed_response)
-        else:
-            return app(environ, start_response)
+        return cors_allowed_response
 
-    return cors_enabled_application
+    def __call__(self, environ, start_response):
+        """Handle an individual request."""
+        origin = environ.get('HTTP_ORIGIN')
+
+        if self.validate_origin(origin):
+            return self.app(
+                environ, self.cors_response_factory(origin, start_response))
+        else:
+            return self.app(environ, start_response)
--- serve_api.py
+++ serve_api.py
@@ -1,11 +1,11 @@
 from wsgiref.simple_server import make_server
 
 from apps.api_endpoint import application
-from middleware.cors_middleware import cors_middleware
+from middleware.cors_middleware import CORSMiddleware
 
 # Run a wsgi server to serve the app
 print "Serving api on port 8000..."
-wrapped_application = cors_middleware(
+wrapped_application = CORSMiddleware(
     application, whitelist=['http://localhost:9000', 'http://localhost:9001'])
 httpd = make_server('localhost', 8000, wrapped_application)
 httpd.serve_forever()
