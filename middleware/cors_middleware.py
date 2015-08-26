984f64b1c9f201a2fb452502ac48c10954c40231
Alan Christopher Thomas <alanctkc@gmail.com>
2015-08-26 13:55:30 -0500

    add whitelist to only allow specific origins cors access

--- middleware/cors_middleware.py
+++ middleware/cors_middleware.py
@@ -1,13 +1,24 @@
+def validate_origin(origin, whitelist):
+    return origin and origin in whitelist
+
+
 def cors_middleware(app):
     print "CORS middleware applied."
 
+    whitelist = ['http://localhost:9000', 'http://localhost:9001']
+
     def cors_enabled_application(environ, start_response):
+
+        origin = environ.get('HTTP_ORIGIN')
 
         def cors_allowed_response(status, response_headers, exc_info=None):
             """This wraps the start_response behavior to add some headers."""
-            response_headers.extend([('Access-Control-Allow-Origin', '*')])
+            response_headers.extend([('Access-Control-Allow-Origin', origin)])
             return start_response(status, response_headers, exc_info)
 
-        return app(environ, cors_allowed_response)
+        if validate_origin(origin, whitelist):
+            return app(environ, cors_allowed_response)
+        else:
+            return app(environ, start_response)
 
     return cors_enabled_application
