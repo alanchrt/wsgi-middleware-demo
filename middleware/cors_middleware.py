bed263cee5b5784e42ed9b04ee75e33322a7d75c
Alan Christopher Thomas <alanctkc@gmail.com>
2015-08-26 13:44:31 -0500

    wrap start_response to add cors header into every response, then continue as usual

--- middleware/cors_middleware.py
+++ middleware/cors_middleware.py
@@ -2,7 +2,12 @@ def cors_middleware(app):
     print "CORS middleware applied."
 
     def cors_enabled_application(environ, start_response):
-        print "Do some stuff here."
-        return app(environ, start_response)
+
+        def cors_allowed_response(status, response_headers, exc_info=None):
+            """This wraps the start_response behavior to add some headers."""
+            response_headers.extend([('Access-Control-Allow-Origin', '*')])
+            return start_response(status, response_headers, exc_info)
+
+        return app(environ, cors_allowed_response)
 
     return cors_enabled_application
